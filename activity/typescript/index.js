d05dc1746f86dc33677b79feed92b2140ba462d5
Esteban Ramirez <esteban.ramirez@5e.cr>
2023-12-24 09:34:18 -0600

    Actividad #3 resolved

--- activity/typescript/index.js
+++ activity/typescript/index.js
@@ -1,4 +1,86 @@
-// Ejercicio 1
+// Ejercicio 1console.log('Ejercicio #1');
+var getPersonInfo = function (person) {
+  var personData = [person.name, person.age, person.profession];
+  return personData;
+};
+var person1 = {
+  name: "Esteban Ramirez",
+  age: 37,
+  profession: "Front End Developer",
+};
+var result = getPersonInfo(person1);
+console.log(result);
 // Ejercicio 2
+console.log("Ejercicio #2");
+function sumOrConcatenate(param1, param2) {
+  if (typeof param1 === "number" && typeof param2 === "number") {
+    // Ambos son números, devuelve la suma
+    return param1 + param2;
+  } else if (typeof param1 === "string" && typeof param2 === "string") {
+    // Ambos son cadenas, devuelve la concatenación con el símbolo '-'
+    return param1 + "-" + param2;
+  } else {
+    // Un número y una cadena, devuelve la concatenación con ':'
+    return "".concat(param1, ":").concat(param2);
+  }
+}
+console.log(sumOrConcatenate(4, 3)); // --> 7
+console.log(sumOrConcatenate(4, "hello")); // --> 4:hello
+console.log(sumOrConcatenate("Hello", "World")); // --> Hello-World
 // Ejercicio 3
+console.log("Ejercicio #3");
+function isCar(vehicle) {
+  return "pressPedal" in vehicle;
+}
+function startVehicle(vehicle) {
+  vehicle.turnOnEngine();
+  if (isCar(vehicle)) {
+    // Si es Car, llamar a pressPedal
+    vehicle.pressPedal();
+  } else {
+    // Si es Motorcycle, llamar a openThrottle
+    vehicle.openThrottle();
+  }
+}
+var car = {
+  tires: 4,
+  turnOnEngine: function () {
+    return console.log("Car engine is on");
+  },
+  pressPedal: function () {
+    return console.log("Pressing the pedal");
+  },
+};
+var motorcycle = {
+  tires: 2,
+  turnOnEngine: function () {
+    return console.log("Motorcycle engine is on");
+  },
+  openThrottle: function () {
+    return console.log("Opening the throttle");
+  },
+};
+startVehicle(car); // Salida esperada: Car engine is on, Pressing the pedal
+startVehicle(motorcycle); // Salida esperada: Motorcycle engine is on, Opening the throttle
 // Ejercicio 4
+console.log("Ejercicio #4");
+function removeFirstEntry(arr) {
+  // Verificar si el array tiene al menos un elemento
+  if (arr.length > 1) {
+    // Devolver el array sin el primer elemento usando slice
+    return arr.slice(1);
+  } else {
+    // Si el array está vacío o tiene solo un elemento, devolver un array vacío
+    return [];
+  }
+}
+var strArray = ["Hello", "World", "Im", "a", "Full", "Stack", "Developer"];
+var numArray = [1, 2, 3, 4, 5, 6, 7];
+var mixedArray = ["Hello", "I", "have", 3, "tasks"];
+var unsupportedArray = [{ name: "Lucas", surname: "Fernandez" }, "Hello", 22];
+var newStrArray = removeFirstEntry(strArray);
+var newNumArray = removeFirstEntry(numArray);
+var newMixedArray = removeFirstEntry(mixedArray);
+console.log(newStrArray); // --> ['World', 'Im', 'a', 'Full', 'Stack', 'Developer'];
+console.log(newNumArray); // --> [2, 3, 4, 5, 6, 7];
+console.log(newMixedArray); // --> ['I', 'have', 3, 'tasks'];
--- activity/typescript/index.ts
+++ activity/typescript/index.ts
@@ -1,7 +1,118 @@
 // Ejercicio 1
+console.log('Ejercicio #1');
+interface Person {
+  name: string;
+  age: number;
+  profession: string;
+}
 
+const getPersonInfo = (person: Person): (string | number)[] => {
+  const personData: (string | number)[] = [
+    person.name,
+    person.age,
+    person.profession,
+  ];
+
+  return personData;
+};
+
+const person1: Person = {
+  name: "Esteban Ramirez",
+  age: 37,
+  profession: "Front End Developer",
+};
+
+const result = getPersonInfo(person1);
+console.log(result);
 // Ejercicio 2
+console.log('Ejercicio #2');
+function sumOrConcatenate(
+  param1: number | string,
+  param2: number | string
+): number | string {
+  if (typeof param1 === 'number' && typeof param2 === 'number') {
+    // Ambos son números, devuelve la suma
+    return param1 + param2;
+  } else if (typeof param1 === 'string' && typeof param2 === 'string') {
+    // Ambos son cadenas, devuelve la concatenación con el símbolo '-'
+    return param1 + '-' + param2;
+  } else {
+    // Un número y una cadena, devuelve la concatenación con ':'
+    return `${param1}:${param2}`;
+  }
+}
+console.log(sumOrConcatenate(4, 3)); // --> 7 
+console.log(sumOrConcatenate(4, "hello")); // --> 4:hello  
+console.log(sumOrConcatenate("Hello", "World")); // --> Hello-World 
 
 // Ejercicio 3
+console.log('Ejercicio #3');
+// Interfaz Car
+interface Car {
+  tires: number;
+  turnOnEngine(): void;
+  pressPedal(): void;
+}
+
+// Interfaz Motorcycle
+interface Motorcycle {
+  tires: number;
+  turnOnEngine(): void;
+  openThrottle(): void;
+}
+
+function isCar(vehicle: Car | Motorcycle): vehicle is Car {
+  return 'pressPedal' in vehicle;
+}
+
+function startVehicle(vehicle: Car | Motorcycle): void {  
+  vehicle.turnOnEngine();
+  
+  if (isCar(vehicle)) {
+    // Si es Car, llamar a pressPedal
+    vehicle.pressPedal();
+  } else {
+    // Si es Motorcycle, llamar a openThrottle
+    vehicle.openThrottle();
+  }
+}
+
+const car: Car = {
+  tires: 4,
+  turnOnEngine: () => console.log('Car engine is on'),
+  pressPedal: () => console.log('Pressing the pedal'),
+};
+
+const motorcycle: Motorcycle = {
+  tires: 2,
+  turnOnEngine: () => console.log('Motorcycle engine is on'),
+  openThrottle: () => console.log('Opening the throttle'),
+};
+
+startVehicle(car); // Salida esperada: Car engine is on, Pressing the pedal
+startVehicle(motorcycle); // Salida esperada: Motorcycle engine is on, Opening the throttle
 
 // Ejercicio 4
+console.log('Ejercicio #4');
+function removeFirstEntry<T>(arr: T[]): T[] {
+  // Verificar si el array tiene al menos un elemento
+  if (arr.length > 1) {
+    // Devolver el array sin el primer elemento usando slice
+    return arr.slice(1);
+  } else {
+    // Si el array está vacío o tiene solo un elemento, devolver un array vacío
+    return [];
+  }
+}
+const strArray: string[] = ['Hello', 'World', 'Im', 'a', 'Full', 'Stack', 'Developer'];
+const numArray: number[] = [1, 2, 3, 4, 5, 6, 7];
+const mixedArray: Array<number|string> = ['Hello', 'I', 'have', 3, 'tasks'];
+const unsupportedArray = [{name: 'Lucas', surname: 'Fernandez'}, 'Hello', 22];
+
+const newStrArray = removeFirstEntry(strArray);
+const newNumArray = removeFirstEntry(numArray);
+const newMixedArray = removeFirstEntry(mixedArray);
+
+console.log(newStrArray); // --> ['World', 'Im', 'a', 'Full', 'Stack', 'Developer'];
+console.log(newNumArray); // --> [2, 3, 4, 5, 6, 7];
+console.log(newMixedArray); // --> ['I', 'have', 3, 'tasks'];
